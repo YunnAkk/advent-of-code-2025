a013f8ef05e64d963612377afaebd87ba5855e57
Yun <aktasyun@students.zhaw.ch>
2026-07-01 11:07:59 +0200

    Improve path resolution safety in get_path_from_root
    Manually handle each path component instead of pushing the raw string, preventing unintended directory traversal. Also drop the dead construct_path helper.

--- src/days/utils.rs
+++ src/days/utils.rs
@@ -1,15 +1,19 @@
-use std::path::PathBuf;
-
-pub fn construct_path(day: u8) -> PathBuf {
-	let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-	path.push("inputs");
-	path.push(format!("day{:02}", day));
-	path.push("input.txt");
-	path
-}
+use std::path::{Component, Path, PathBuf};
 
 pub fn get_path_from_root(relative_path: &str) -> PathBuf {
-	let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-	path.push(relative_path);
-	path
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let relative_path = Path::new(relative_path);
+
+    for component in relative_path.components() {
+        match component {
+            Component::Normal(c) => path.push(c),
+            Component::ParentDir => {
+                path.pop();
+            }
+            Component::RootDir | Component::Prefix(_) => {}
+            Component::CurDir => {}
+        }
+    }
+
+    path
 }
