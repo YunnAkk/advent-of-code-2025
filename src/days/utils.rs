6b419bb432415e0fca11a56d5d858555c8fa52a4
Yun <aktasyun@students.zhaw.ch>
2026-07-01 11:51:39 +0200

    Prevent get_path_from_root from traversing above project root

--- src/days/utils.rs
+++ src/days/utils.rs
@@ -1,14 +1,17 @@
 use std::path::{Component, Path, PathBuf};
 
 pub fn get_path_from_root(relative_path: &str) -> PathBuf {
-    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let mut path = root.to_path_buf();
     let relative_path = Path::new(relative_path);
 
     for component in relative_path.components() {
         match component {
             Component::Normal(c) => path.push(c),
             Component::ParentDir => {
-                path.pop();
+                if path != root {
+                    path.pop();
+                }
             }
             Component::RootDir | Component::Prefix(_) => {}
             Component::CurDir => {}
