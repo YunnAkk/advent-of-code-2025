11aca70c00c7e09b0db6c110b0ec2e2e4c1c9d11
Yun <aktasyun@students.zhaw.ch>
2026-07-03 12:50:02 +0200

    Add first rough draft of day02 range parsing logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::io::Read;
+
 mod days;
 
 fn main() {
-    let path = days::utils::get_path_from_root("inputs/day01/input.txt");
-    let result = days::day01::count_dial_zero_hits(&path, 50).unwrap();
-    println!("{result}");
+    let path = days::utils::get_path_from_root("test_inputs/day02/aoc_example.txt");
+    // let result = days::day01::count_dial_zero_hits(&path, 50).unwrap();
+
+    days::day02::func_day2(&path);
 }
