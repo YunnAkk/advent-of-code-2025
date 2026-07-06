1a35a70008df68d25b5cdbc278ffe20af8251f26
Yun <aktasyun@students.zhaw.ch>
2026-07-06 18:18:55 +0200

    Add day02 unit tests

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -58,7 +58,7 @@ fn process_chunk(ranges_str: &str, invalid_ids: &mut Vec<i64>) {
     }
 }
 
-pub fn sum_invalid_ids_in_ranges(path: &PathBuf) {
+pub fn sum_invalid_ids_in_ranges(path: &PathBuf) -> i64 {
     let mut reader = buffered_reader(path).unwrap();
     let mut invalid_ids: Vec<i64> = Vec::new();
     let mut pending_bytes: Vec<u8> = Vec::new();
@@ -95,5 +95,56 @@ pub fn sum_invalid_ids_in_ranges(path: &PathBuf) {
         }
     }
 
-    let total: i64 = invalid_ids.iter().sum();
+    invalid_ids.iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::days::utils::get_path_from_root;
+
+    mod invalid_ids_single_sequence {
+        use super::*;
+
+        #[test]
+        fn aoc_example() {
+            let path = get_path_from_root("test_inputs/day02/aoc_example.txt");
+            assert_eq!(sum_invalid_ids_in_ranges(&path), 1227775554);
+        }
+
+        #[test]
+        fn left_equal_right() {
+            let path = get_path_from_root("test_inputs/day02/left_equal_right.txt");
+            assert_eq!(sum_invalid_ids_in_ranges(&path), 48985);
+        }
+
+        #[test]
+        fn left_greater() {
+            let path = get_path_from_root("test_inputs/day02/left_greater_right.txt");
+            assert_eq!(sum_invalid_ids_in_ranges(&path), 4444);
+        }
+
+        #[test]
+        fn left_lesser() {
+            let path = get_path_from_root("test_inputs/day02/left_less_right.txt");
+            assert_eq!(sum_invalid_ids_in_ranges(&path), 2222);
+        }
+
+        #[test]
+        fn no_invalid() {
+            let path = get_path_from_root("test_inputs/day02/no_invalid.txt");
+            assert_eq!(sum_invalid_ids_in_ranges(&path), 0);
+        }
+
+        #[test]
+        fn single_invalid() {
+            let path = get_path_from_root("test_inputs/day02/single_invalid.txt");
+            assert_eq!(sum_invalid_ids_in_ranges(&path), 11);
+        }
+    }
+
+    mod invalid_ids_repeated_sequence {
+
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use std::io::Read;
-
 mod days;
 
 fn main() {
-    let path = days::utils::get_path_from_root("test_inputs/day02/aoc_example.txt");
-    // let result = days::day01::count_dial_zero_hits(&path, 50).unwrap();
+    let day01_path = days::utils::get_path_from_root("inputs/day01/input.txt");
+    let day02_path = days::utils::get_path_from_root("test_inputs/day02/aoc_example.txt");
 
-    days::day02::func_day2(&path);
+    let day01_part1 = days::day01::count_dial_zero_hits(&day01_path, 50);
+    let day01_part2 = days::day01::count_dial_zero_passes(&day01_path, 50);
+    println!("==========================================");
+    println!("Result for Day 1 Part 1: {:?}", day01_part1);
+    println!("Result for Day 1 Part 1: {:?}", day01_part2);
+
+    let day02_part1 = days::day02::sum_invalid_ids_in_ranges(&day02_path);
+    println!("==========================================");
+    println!("Result for Day 2 Part 2: {:?}", day02_part1);
 }
